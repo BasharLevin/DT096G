afaa8ab48c483ed399fac21097b241683b8cd3dc
BasharLevin <basharaboalneaj@outlook.com>
2024-03-05 19:10:08 +0100

    prototype fixed

--- labbdos/prototyp.js
+++ labbdos/prototyp.js
@@ -1,24 +1,23 @@
-Array.prototype.test = function() {
-    let idx = 0;
-    let arr = this;
-    return {
-        has_next: function() {
-            return idx < arr.length;
-        },
-        next: function() {
-            if (this.has_next()) {
-                return arr[idx++];
-            } else {
-                console.log("Element does not exist.");
-                return null;
-            }
-        }
-    };
-};
 
-const array_list = [6, 9, 10, 20, 33];
-let it = array_list.test();
+class array{
+    constructor(my_array, iter) {
+        this.iter = iter;
+        this.my_array = my_array;
+    }
+    has_next = function(){
+        return this.iter < this.my_array.length;
+    }
 
-while (it.has_next()) {
-    console.log(it.next());
+    next = function(){
+        return this.my_array[this.iter++];
+    }
+
 }
+Array.prototype.iter = function () {
+    const it = new array(this, 0)
+    while (it.has_next()){
+        console.log(it.next());
+    }
+}
+arr =[1,69,420,666];
+arr.iter();
