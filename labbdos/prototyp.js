26ae0e8eec00e991672950edd8f9be3cbebac6cb
BasharLevin <basharaboalneaj@outlook.com>
2024-03-13 19:48:00 +0100

    prototype fixed

--- labbdos/prototyp.js
+++ labbdos/prototyp.js
@@ -7,16 +7,15 @@ class array{
     has_next = function(){
         return this.iter < this.my_array.length;
     }
-
     next = function(){
         return this.my_array[this.iter++];
     }
-
 }
 Array.prototype.iter = function () {
     const it = new array(this, 0)
-    while (it.has_next()){
+    while (it.has_next()) {
         console.log(it.next());
     }
-arr =[1,69,420,666];
-arr.iter();
+    arr = [1, 69, 420, 666];
+    arr.iter();
+}
